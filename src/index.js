7bdd279aa3485f0adfb448e56f5b20cac2ae67f9
Yauheniya Viarbitskaya <evgeniya.verbiczkaya98@gmail.com>
2018-09-17 22:12:36 +0300

    h

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    var loveTrianglesSum = 0;
+    for (var i = 0; i < preferences.length; i++) {
+        var firstLover = preferences[i];
+        var secondLover = preferences[firstLover - 1];
+        if (preferences[i] !== i + 1 && preferences[secondLover - 1] - 1 == i) {
+            loveTrianglesSum++;
+            preferences[i] = 0;
+            preferences[firstLover - 1] = 0;
+            preferences[secondLover - 1] = 0;
+        }
+    }
+    return loveTrianglesSum;
+}
